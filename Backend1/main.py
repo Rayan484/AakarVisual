3ce3e9a3925a09311696ea3f159f32a2beadce2a
Omprakash Choudhary <omprakash choudhary>
2024-02-06 14:40:46 +0530

    final change

--- Backend1/main.py
+++ Backend1/main.py
@@ -5,18 +5,17 @@ import flask
 import numpy as np
 import cv2
 import os
-import urllib.request
 import onnxruntime
-urllib.request.urlretrieve("https://firebasestorage.googleapis.com/v0/b/aakar-21675.appspot.com/o/sam_vit_b_encoder.onnx?alt=media&token=51e26e1f-eb6b-4103-84f5-6b5a53cc5f43", "sam_vit_b_encoder.onnx")
-encoder_path = "sam_vit_b_encoder.onnx"
-encoder_session = onnxruntime.InferenceSession(encoder_path, providers=['CPUExecutionProvider'])
+encoder_path = "models/sam_vit_b_encoder.onnx"
+encoder_session = onnxruntime.InferenceSession(
+    encoder_path, providers=['CPUExecutionProvider'])
 app = Flask(__name__)
 CORS(app, expose_headers=["Content-Disposition"])
 
 
 @app.route('/', methods=['GET'])
+@cross_origin(origin='*')
 def home():
-    
     return "Hello, World !!"
 
 @app.route('/test',methods=['POST'])
@@ -56,9 +55,7 @@ def getembedding1():
     if output is not None:
         output = output[0]
     image_embedding = output
-    response=flask.jsonify(image_embedding.tolist())
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    return response
+    return flask.jsonify(image_embedding.tolist())
 
 
 if (__name__ == '__main__'):
